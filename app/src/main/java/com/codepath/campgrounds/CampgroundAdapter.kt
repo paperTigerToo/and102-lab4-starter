862d73b409b00572c3cf0695bd1bbcddff5b0cde
paperTigerToo <zacharykochemail@gmail.com>
2026-02-19 14:12:52 -0500

    can touch

--- app/src/main/java/com/codepath/campgrounds/CampgroundAdapter.kt
+++ app/src/main/java/com/codepath/campgrounds/CampgroundAdapter.kt
@@ -1,6 +1,7 @@
 package com.codepath.campgrounds
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -55,10 +56,12 @@ class CampgroundAdapter(private val context: Context,private val campgrounds: Li
 
         override fun onClick(v: View?) {
             // TODO: Get selected campground
-
+            val campground = campgrounds[absoluteAdapterPosition]
 
             // TODO: Navigate to Details screen and pass selected campground
-
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(CAMPGROUND_EXTRA, campground)
+            context.startActivity(intent)
         }
     }
 }
